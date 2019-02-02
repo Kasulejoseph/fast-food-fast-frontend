2e1abb0956388943f2734687a3ad59c696f8b89f
kasulejoseph <kasule06joseph@gmail.com>
2019-02-02 19:56:16 +0300

    chore(setup-test):configure test environment for the application
    setup travis and coveralls
    configure jest as the test runner
    
    [Delivers #163670361]

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,27 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(bower_components)/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        options: { presets: ["@babel/env"] },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader']
+      },
+
     ]
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
